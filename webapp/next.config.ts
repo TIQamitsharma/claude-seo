dfb2309ad5d91c36a8c994cf6c25624ad6d0bdd6
TIQamitsharma <amit.sharma@technocratiq.com>
2026-03-15 10:54:50 +0530

    Updated next.config.ts

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL!,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   },
-  turbopack: {
-    root: __dirname,
-  },
 };
 
 export default nextConfig;
